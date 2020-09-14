89effa295342f087d89a2eafb71b6255c9e6ca63
erikalvar <erikalvarbass@gmail.com>
2020-09-14 18:34:11 -0400

    adds reviews new vue

---
+++ src/views/ReviewsNew.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="reviews-new">
+    <h1>Add a Review</h1>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      newReviewProfessorId: "",
+      newReviewRating: "",
+      newReviewText: "",
+    };
+  },
+  methods: {
+    createReview: function () {
+      var params = {
+        professor_id: this.newReviewProfessorId,
+        rating: this.newReviewRating,
+        text: this.newReviewText,
+      };
+      axios
+        .post("/api/reviews", params)
+        .then((response) => {
+          console.log("Successfully added", response.data);
+          // this.reviews.push(response.data);
+        })
+        .then((response) => {
+          // this.$router.push("/professor/:id");
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
